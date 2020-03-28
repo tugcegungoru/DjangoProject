5aa9569b3d120b03a78d103605f2a24e5bae4b65
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-03-28 18:54:42 +0200

    Proje İlk Upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Django Araá Alçm Satçm Projesi" 
---
+++ car/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False' , 'Hayır'),
+    )
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self',blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_up = models.DateTimeField(auto_now_add=True)
+    update_up = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Car(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    SITUATION = (
+        ('True','Used'),
+        ('False','New'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1500)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    year = models.IntegerField()
+    fuel = models.CharField(max_length=15)
+    gear = models.CharField(max_length=20)
+    km = models.IntegerField()
+    motorpower = models.IntegerField()
+    color = models.CharField(max_length=20)
+    situation = models.CharField(max_length=10, choices=SITUATION)
+    status = models.CharField(max_length=10, choices=STATUS)
+    detail = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
