6b05dbf9403d3cd3c3bddddcec9b9dd5794ffe30
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-04-04 19:39:29 +0200

    Adminde Yüklenen Resimlerin Gösterilmesi

--- car/admin.py
+++ car/admin.py
@@ -8,16 +8,21 @@ class CarImageInline(admin.TabularInline):
     extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status','image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['status']
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category','price','year', 'fuel','gear','km','situation','status']
+    list_display = ['title', 'category','price','image_tag','year', 'fuel','gear','km','situation','status']
+    readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
     inlines = [CarImageInline]
 
+
 class ImagesAdmin(admin.ModelAdmin):
-    list_display = ['title', 'car', 'image']
+    list_display = ['title', 'car', 'image_tag']
+    readonly_fields = ('image_tag',)
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Car, CarAdmin)
 admin.site.register(Images, ImagesAdmin)
--- car/models.py
+++ car/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -18,6 +21,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
 
 class Car(models.Model):
     STATUS = (
@@ -49,6 +57,10 @@ class Car(models.Model):
     def __str__(self):
         return self.title
 
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
 class Images(models.Model):
     car = models.ForeignKey(Car,on_delete=models.CASCADE)
     title = models.CharField(max_length=150, blank=True)
@@ -56,3 +68,7 @@ class Images(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
