c3a0801d752077d03c6cdec8af1372771be80e64
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-04-19 02:06:45 +0300

    Contact Us Form (Bize Ulaşın Formu)

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
+from django.forms import ModelForm, TextInput, Textarea
+
+
 class Setting(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -32,5 +35,32 @@ class Setting(models.Model):
     def __str__(self):
         return self.title
 
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+    )
+    name = models.CharField(blank=True, max_length=20)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name','email','subject','message']
+        widgets = {
+            'name' : TextInput(attrs={'class': 'form-group','placeholder': 'Name & Surname'}),
+            'subject': TextInput(attrs={'class': 'form-group', 'placeholder': 'Subject'}),
+            'email': TextInput(attrs={'class': 'form-group', 'placeholder': 'Email Address'}),
+            'message': Textarea(attrs={'class': 'form-group', 'placeholder': 'Your Message','rows':'5'}),
+        }
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
+
 
 def index(request):
     setting = Setting.objects.get(pk=1)
@@ -11,15 +13,29 @@ def index(request):
 
 def hakkimizda(request):
     setting = Setting.objects.get(pk=1)
-    context= {'setting': setting, 'page':'hakkimizda'}
+    context= {'setting': setting}
     return render(request, 'hakkimizda.html', context)
 
 def referanslar(request):
     setting = Setting.objects.get(pk=1)
-    context= {'setting': setting, 'page':'referanslar'}
+    context= {'setting': setting}
     return render(request, 'referanslar.html', context)
 
-def iletisim(request):
-    setting = Setting.objects.get(pk=1)
-    context= {'setting': setting, 'page':'iletisim'}
+def iletisim(request):      #formu kaydetmek için
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız başarı ile gönderilmiştir.")
+            return HttpResponseRedirect('/iletisim')
+
+    setting = Setting.objects.get(pk=1)   #forma ulaşmak için
+    form = ContactFormu()
+    context = {'setting': setting, 'form':form}
     return render(request, 'iletisim.html', context)
