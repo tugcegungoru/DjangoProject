259af8c4aaf90d5019fb03e822a6553469a1186b
tucegungoru@gmail.com <tucegungoru@gmail.com>
2020-05-11 19:07:43 +0300

    Anasayfada Kullanıcı Giriş ve Çıkış İşlemleri

--- myProject1/urls.py
+++ myProject1/urls.py
@@ -31,7 +31,9 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_cars, name='category_cars'),
     path('car/<int:id>/<slug:slug>/', views.car_detail, name='car_detail'),
-    path('search/', views.car_search, name='car_search')
+    path('search/', views.car_search, name='car_search'),
+    path('logout/', views.logout_view,name='logout_view'),
+    path('login/', views.login_view,name='login_view')
 ]
 if settings.DEBUG: #new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
